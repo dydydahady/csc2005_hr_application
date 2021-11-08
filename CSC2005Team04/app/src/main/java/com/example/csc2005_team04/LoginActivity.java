cdb5cccf32e0d9335cbb4b0b7afc48597a2c472b
dydydahady <abdulhadyzaydie@gmail.com>
2021-11-08 20:50:24 +0800

    Integrated Login Page with Main Application. Added Firebase Auth for Login, App will quit after 3 failed attempts

---
+++ CSC2005Team04/app/src/main/java/com/example/csc2005_team04/LoginActivity.java
@@ -0,0 +1,118 @@
+package com.example.csc2005_team04;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
+    private TextView forgotPassword;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+
+        signIn = (Button) findViewById(R.id.signIn);
+        signIn.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.username_input);
+        editTextPassword = (EditText) findViewById(R.id.pass);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        forgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        forgotPassword.setOnClickListener(this);
+
+
+    }
+
+//    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.signIn:
+                userLogin();
+                break;
+
+            case R.id.forgotPassword:
+                startActivity(new Intent(this, forgotPassword.class));
+                break;
+        }
+    }
+
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Min password length is 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    //redirect to main dashboard page
+                    Toast.makeText(LoginActivity.this, "Successful login", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    count++;
+                    if(count == 3){
+                        Toast.makeText(LoginActivity.this, "5 Failed Attempts. Exiting...", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                    Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
